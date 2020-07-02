14452ebe8806233dcf0c84bc068306b6ed9335c1
Diego Nogueira <diegoalvareznogueira@gmail.com>
2020-07-02 14:22:01 -0300

    Initial commit

---
+++ lib/skeleton_query.ex
@@ -0,0 +1,18 @@
+defmodule SkeletonQuery do
+  @moduledoc """
+  Documentation for `SkeletonQuery`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SkeletonQuery.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
